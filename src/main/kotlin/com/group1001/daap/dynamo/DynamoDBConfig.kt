8e40554f9c13f98a987a3ccf48e3a6a16090aed2
Robbie Diaz <robert.diaz@group1001.com>
2020-01-27 14:29:47 -0500

    Added another property that allows for overriding the dynamo client to point to a non-standard dynamo instance

--- src/main/kotlin/com/group1001/daap/dynamo/DynamoDBConfig.kt
+++ src/main/kotlin/com/group1001/daap/dynamo/DynamoDBConfig.kt
@@ -10,17 +10,32 @@ import org.springframework.context.annotation.Configuration
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Profile
 import org.springframework.core.Ordered
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials
+import software.amazon.awssdk.regions.Region
 import software.amazon.awssdk.services.dynamodb.DynamoDbClient
+import java.net.URI
 
 @Configuration
 @EnableConfigurationProperties(DynamoProperties::class)
 @Import(EntityRegistry::class)
 @AutoConfigureOrder(Ordered.HIGHEST_PRECEDENCE)
 @Profile("!test")
-class DynamoDBConfig {
+class DynamoDBConfig(val properties: DynamoProperties) {
     @Bean
     @ConditionalOnMissingBean
-    fun dynamoClient(): DynamoDbClient = DynamoDbClient.create()
+    fun dynamoClient(): DynamoDbClient {
+        // If we have an override specified, use it, otherwise, pull from the environment
+        val url = properties.endpointOverride
+        return if (url != null) {
+            DynamoDbClient.builder()
+                .endpointOverride(URI(url)) // These credentials are hard coded because that's what the docker-compose expects
+                .credentialsProvider { AwsBasicCredentials.create("local", "local") }
+                .region(Region.US_EAST_2)
+                .build()
+        } else {
+            DynamoDbClient.create()
+        }
+    }
 
     @Bean
     fun tableCreationService(properties: DynamoProperties, client: DynamoDbClient) =
@@ -31,7 +46,8 @@ class DynamoDBConfig {
 @ConfigurationProperties(prefix = "com.group1001.dynamo")
 class DynamoProperties(
     val tableCreationMode: TableCreationMode,
-    val classPackage: String
+    val classPackage: String,
+    val endpointOverride: String? = null
 ) {
     enum class TableCreationMode {
         NONE,
